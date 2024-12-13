01d7cc5264ea14659094d43284f38daa03cc1ab5
Abdulaziz Fatkhullaev <actuallyabdulaziz@gmail.com>
2024-12-14 04:35:52 +0500

    Настройка server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');  // Добавление CORS
 const app = express();
 
 // Используем порт из переменной окружения, если он существует, или 3000 по умолчанию
 const PORT = process.env.PORT || 3000;
+
+// Разрешаем CORS
+app.use(cors());
 
 // Позволяет обрабатывать JSON из запроса
 app.use(express.json());
@@ -15,14 +19,13 @@ app.post('/feedback', (req, res) => {
     res.status(200).send('Обратная связь получена!');
 });
 
-// Настройка для обслуживания статических файлов (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
+// Настройка для обслуживания статических файлов из текущей папки
+app.use(express.static(path.join(__dirname)));
 
 // Обработка главной страницы (для GET-запросов на /)
 app.get('/', (req, res) => {
-    res.send('Добро пожаловать на мой сервер!');
+    res.sendFile(path.join(__dirname, 'index.html'));  // Убедитесь, что index.html находится в той же папке, что и server.js
 });
-
 
 // Запуск сервера
 app.listen(PORT, () => {
